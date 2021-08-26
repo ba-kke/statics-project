2ef981883958f50dc0ccaa74e8ffcbbca679839b
ba-kke <obakebake1223@gmail.com>
2021-08-26 14:10:38 +0900

    modified php

--- php/mail.php
+++ php/mail.php
@@ -30,7 +30,7 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 //---------------------------　必須設定　必ず設定してください　-----------------------
 
 //サイトのトップページのURL　※デフォルトでは送信完了後に「トップページへ戻る」ボタンが表示されますので
-$site_top = "../layout/contact.html";
+$site_top = "https://ba-kke.github.io/statics-project";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
 $to = "obakebake1223@gmail.com";
@@ -52,7 +52,7 @@ $Referer_check = 1;
 
 //リファラチェックを「する」場合のドメイン ※設置するサイトのドメインを指定して下さい。
 //もしこの設定が間違っている場合は送信テストですぐに気付けます。
-$Referer_check_domain = "ba-kke.github.io";
+// $Referer_check_domain = "ba-kke.github.io";
 
 /*セッションによるワンタイムトークン（CSRF対策、及びスパム防止）(する=1, しない=0)
 ※ただし、この機能を使う場合は↓の送信確認画面の表示が必須です。（デフォルトではON（1）になっています）
@@ -84,7 +84,7 @@ $confirmDsp = 1;
 $jumpPage = 0;
 
 // 送信完了後に表示するページURL（上記で1を設定した場合のみ）※httpから始まるURLで指定ください。（相対パスでも基本的には問題ないです）
-$thanksPage = "../layout/contact.html";
+$thanksPage = "https://ba-kke.github.io/statics-project/";
 
 // 必須入力項目を設定する(する=1, しない=0)
 $requireCheck = 1;
